e4f2e7fb5bcbadb356e85adc344d201cd7a1412c
RedEnergy <forwot163@gmail.com>
2016-02-07 23:15:31 +0400

    Added /online route which output data about current online

--- src/main/kotlin/ru/redenergy/warden/OnlineApplication.kt
+++ src/main/kotlin/ru/redenergy/warden/OnlineApplication.kt
@@ -2,6 +2,9 @@ package ru.redenergy.warden
 
 import ch.jamiete.mcping.MinecraftPing
 import ch.jamiete.mcping.MinecraftPingOptions
+import com.github.salomonbrys.kotson.jsonArray
+import com.github.salomonbrys.kotson.jsonObject
+import com.google.gson.GsonBuilder
 import com.j256.ormlite.dao.Dao
 import com.j256.ormlite.dao.DaoManager
 import com.j256.ormlite.jdbc.JdbcConnectionSource
@@ -9,6 +12,7 @@ import com.j256.ormlite.table.TableUtils
 import ru.redenergy.warden.config.Server
 import ru.redenergy.warden.entity.ServerOnline
 import ru.redenergy.warden.entity.TotalOnline
+import spark.Spark
 import java.io.IOException
 import java.sql.Timestamp
 import java.util.concurrent.Executors
@@ -16,6 +20,7 @@ import java.util.concurrent.TimeUnit
 
 class OnlineApplication {
 
+    val gson = GsonBuilder().create()
     val connection = JdbcConnectionSource("jdbc:mysql://localhost:3306/test", "root", "mysql");
     val scheduler = Executors.newScheduledThreadPool(5)
     val servers = arrayListOf<Server>()
@@ -26,6 +31,7 @@ class OnlineApplication {
         this.servers.addAll(servers)
         TableUtils.createTableIfNotExists(connection, ServerOnline::class.java)
         TableUtils.createTableIfNotExists(connection, TotalOnline::class.java)
+        registerRoutes()
         this.scheduler.scheduleAtFixedRate({ tickServers() }, 1, 60, TimeUnit.SECONDS)
     }
 
@@ -35,7 +41,7 @@ class OnlineApplication {
             record?.run { onlineDao.createOrUpdate(this) }
         }
         val total = records.sumBy { it!!.online }
-        totalDao.create(TotalOnline(time = Timestamp(System.currentTimeMillis()), online = total))
+        totalDao.create(TotalOnline(time = System.currentTimeMillis(), online = total))
 
         println("Successfully pinged ${records.size} servers with total online $total players, ${servers.size - records.size} unsuccessful.")
     }
@@ -43,10 +49,21 @@ class OnlineApplication {
     fun fetchServerOnline(server: Server): ServerOnline? =
         try {
             val ping = MinecraftPing().getPing(MinecraftPingOptions().apply { hostname = server.ip; port = server.port })
-            ServerOnline(server.id, Timestamp(System.currentTimeMillis()), ping.players.online)
+            ServerOnline(server.id, System.currentTimeMillis(), ping.players.online)
         } catch(ex: IOException){
             null
         }
+
+    fun registerRoutes(){
+        Spark.get("/online", {req, res ->
+            val online = onlineDao.queryForAll()
+            jsonObject(
+                "servers" to gson.toJsonTree(online),
+                "total" to online.sumBy { it.online }
+            )
+        })
+    }
+
 
 
 }
