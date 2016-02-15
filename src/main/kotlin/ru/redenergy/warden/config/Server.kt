9eadeba0aca28dd4eb8c6d8bf4471a3ab4386d10
RedEnergy <forwot163@gmail.com>
2016-02-15 21:52:43 +0400

    Server class is now data class

--- src/main/kotlin/ru/redenergy/warden/config/Server.kt
+++ src/main/kotlin/ru/redenergy/warden/config/Server.kt
@@ -1,3 +1,3 @@
 package ru.redenergy.warden.config
 
-class Server(var id: String, var name: String, var ip: String, var port: Int)
+data class Server(var id: String, var name: String, var ip: String, var port: Int)
